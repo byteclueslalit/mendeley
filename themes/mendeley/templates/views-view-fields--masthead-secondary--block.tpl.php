cf570a9b87adea32454d9daf82ab0415d466760c
ankitasharma-20feb <ankita.sharma@byteclues.com>
2016-03-02 15:59:00 +0530

    Masthead Primary, Secondary Content type
    Masthead Primary, Secondary Content types created.

--- themes/mendeley/templates/views-view-fields--masthead-secondary--block.tpl.php
+++ themes/mendeley/templates/views-view-fields--masthead-secondary--block.tpl.php
@@ -1,35 +1,35 @@
 <?php 
 $node=node_load($row->nid);   
 $field_secondary_logo=field_get_items('node',$node,'field_secondary_logo');
-$field_header_links=field_get_items('node',$node,'field_header_links');
+$field_primary_links=field_get_items('node',$node,'field_primary_links');
 $field_secondary_links=field_get_items('node',$node,'field_secondary_links');   
 ?> 
 <nav class="nav nav-primary">
 <div class="logo-wrapper" style="float:left;">
 		<img src="<?php echo file_create_url($field_secondary_logo[0]['uri']);?>" alt="<?php echo $field_secondary_logo[0]['alt'];?>" width="100%" />
 </div>
-<a href="<?php echo $field_header_links[0]['url'];?>" style="margin:5px 15px;padding:0;" class="nav--item">
-	<?php echo $field_header_links[0]['title'];?>
+<a href="<?php echo $field_primary_links[0]['url'];?>" style="margin:5px 15px;padding:0;" class="nav--item">
+	<?php echo $field_primary_links[0]['title'];?>
 </a>
-<a href="<?php echo $field_header_links[1]['url'];?>" style="margin:5px 15px;padding:0;" class="nav--item">
-	<?php echo $field_header_links[1]['title'];?>
+<a href="<?php echo $field_primary_links[1]['url'];?>" style="margin:5px 15px;padding:0;" class="nav--item">
+	<?php echo $field_primary_links[1]['title'];?>
 </a>
-<a href="<?php echo $field_header_links[2]['url'];?>" style="margin:5px 15px;padding:0;" class="nav--item">
-	<?php echo $field_header_links[2]['title'];?>
+<a href="<?php echo $field_primary_links[2]['url'];?>" style="margin:5px 15px;padding:0;" class="nav--item">
+	<?php echo $field_primary_links[2]['title'];?>
 </a>
-<a href="<?php echo $field_header_links[3]['url'];?>" style="margin:5px 15px;padding:0;" class="nav--item">
-	<?php echo $field_header_links[3]['title'];?>
+<a href="<?php echo $field_primary_links[3]['url'];?>" style="margin:5px 15px;padding:0;" class="nav--item">
+	<?php echo $field_primary_links[3]['title'];?>
 </a>
 </nav>
 <nav class="nav nav-secondary">
-<a href="<?php echo $field_header_links[4]['url'];?>" class="nav--item">
-	<?php echo $field_header_links[4]['title'];?>
+<a href="<?php echo $field_primary_links[4]['url'];?>" class="nav--item">
+	<?php echo $field_primary_links[4]['title'];?>
 </a>
-<a href="<?php echo $field_header_links[5]['url'];?>" class="nav--item signin-icon">
-	<?php echo $field_header_links[5]['title'];?>
+<a href="<?php echo $field_primary_links[5]['url'];?>" class="nav--item signin-icon">
+	<?php echo $field_primary_links[5]['title'];?>
 </a>
-<a href="<?php echo $field_header_links[6]['url'];?>" class="nav--item btn">
-	<?php echo $field_header_links[6]['title'];?>
+<a href="<?php echo $field_primary_links[6]['url'];?>" class="nav--item btn">
+	<?php echo $field_primary_links[6]['title'];?>
 </a>
 </nav>
 <nav style="float:left;width:100%;font-size:12px;">
